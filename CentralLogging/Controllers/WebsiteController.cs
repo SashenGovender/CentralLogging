ef46aa4200294197a2573fb1407e6ceb4f658fb4
Sashen <sashen50@gmail.com>
2020-08-29 13:31:37 +0200

    added some console logs to see thread id

--- CentralLogging/Controllers/WebsiteController.cs
+++ CentralLogging/Controllers/WebsiteController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 
+
 namespace CentralLogging.Controllers
 {
   [Route("website")]
@@ -30,6 +31,7 @@ namespace CentralLogging.Controllers
     {
       return new string[] { "value1", "value2" };
     }
+
 
 
     [HttpPost]
@@ -55,24 +57,33 @@ namespace CentralLogging.Controllers
     [HttpPost("count")]
     public async Task<IActionResult> QueryWebsiteCount([FromBody] MessageRequest request)
     {
+      PrintThreadIdToConsole("start");
+
       if (string.IsNullOrWhiteSpace(request.Website))
       {
         return BadRequest();
       }
 
       var response = await _client.GetAsync(request.Website);
+      PrintThreadIdToConsole("got response");
       if (response.StatusCode != HttpStatusCode.OK)
       {
         return BadRequest(response);
       }
 
       var html = await response.Content.ReadAsStringAsync();
+      PrintThreadIdToConsole("got content");
 
       var letterCounts = await Task.Run(() => _wordCounter.CountPerLetter(html));
 
+      PrintThreadIdToConsole("after word count");
       var jsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(letterCounts);
       return Ok(jsonResult);
     }
 
+    private void PrintThreadIdToConsole(string message)
+    {
+      Console.WriteLine($"{message} - ThreadId - {Thread.CurrentThread.ManagedThreadId.ToString()} ");
+    }
   }
 }
