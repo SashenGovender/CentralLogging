c0b6d3e6accb21c2eaad495ec65c6f49248fee80
Sashen <sashen50@gmail.com>
2020-08-29 13:39:13 +0200

    added a logging context to store log messages across threads

--- CentralLogging/Controllers/WebsiteController.cs
+++ CentralLogging/Controllers/WebsiteController.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using CentralLogging.Models;
+using CentralLogging.Observability;
 using CentralLogging.Processor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
-
 
 namespace CentralLogging.Controllers
 {
@@ -21,23 +19,30 @@ namespace CentralLogging.Controllers
   {
     private static readonly HttpClient _client = new HttpClient(); // how do I make this once per application?
     private readonly IWordCounter _wordCounter;
+
     public WebsiteController(IWordCounter wordCounter)
     {
       _wordCounter = wordCounter;
     }
 
+    /// <summary>
+    ///  A test get endpoint to check if the api is up
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     public IEnumerable<string> Get()
     {
       return new string[] { "value1", "value2" };
     }
 
-
-
+    /// <summary>
+    /// Query the specified website for its html content
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     [HttpPost]
     public async Task<IActionResult> QueryWebsite([FromBody] MessageRequest request)
     {
-
       if (string.IsNullOrWhiteSpace(request.Website))
       {
         return BadRequest();
@@ -54,10 +59,17 @@ namespace CentralLogging.Controllers
       return Ok(html);
     }
 
+    /// <summary>
+    ///  Query the specified website for the count of each character
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     [HttpPost("count")]
     public async Task<IActionResult> QueryWebsiteCount([FromBody] MessageRequest request)
     {
       PrintThreadIdToConsole("start");
+
+      LogContext.Context.AddLog($"{LogLevel.Information} - Received Message Request - {JsonSerializer.Serialize(request)}");
 
       if (string.IsNullOrWhiteSpace(request.Website))
       {
@@ -66,6 +78,7 @@ namespace CentralLogging.Controllers
 
       var response = await _client.GetAsync(request.Website);
       PrintThreadIdToConsole("got response");
+
       if (response.StatusCode != HttpStatusCode.OK)
       {
         return BadRequest(response);
@@ -75,8 +88,8 @@ namespace CentralLogging.Controllers
       PrintThreadIdToConsole("got content");
 
       var letterCounts = await Task.Run(() => _wordCounter.CountPerLetter(html));
-
       PrintThreadIdToConsole("after word count");
+
       var jsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(letterCounts);
       return Ok(jsonResult);
     }
---
+++ CentralLogging/Observability/LogContext.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CentralLogging.Observability
+{
+  public class LogContext
+  {
+    private static readonly object _lock = new object();
+    private static readonly AsyncLocal<LogContext> _logContext = new AsyncLocal<LogContext>();
+    private readonly ConcurrentQueue<string> _logQueue= new ConcurrentQueue<string>(); //order is important right?
+    private LogContext() { }
+
+    public static LogContext Context
+    {
+      get
+      {
+        if (_logContext.Value == null)
+        {
+          lock (_lock)
+          {
+            if (_logContext.Value == null)
+            {
+              _logContext.Value = new LogContext();
+            }
+          }
+        }
+
+        return _logContext.Value;
+      }
+    }
+
+    public void AddLog(string message)
+    {
+      _logQueue.Enqueue(message);
+    }
+
+    public IReadOnlyList<string> GetLogs(/*LogLevel logLevel*/)
+    {
+      return _logQueue.ToList();
+
+     // return _logQueue.Where(element => element.Contains(logLevel.ToString())).ToList();
+    }
+
+
+
+
+
+
+
+    /*
+     * Nlog Log Levels - https://github.com/NLog/NLog/wiki/Configuration-file#log-levels
+     * Level -	Typical Use
+     * Fatal -	Something bad happened; application is going down
+     * Error -	Something failed; application may or may not continue
+     * Warn  -	Something unexpected; application will continue
+     * Info  -	Normal behavior like mail sent, user updated profile etc.
+     * Debug -	For debugging; executed query, user authenticated, session expired
+     * Trace -	For trace debugging; begin method X, end method X
+     * 
+     * 
+     * Microsoft Log Levels - https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1#log-level
+     * 
+     * None       - 6 - Specifies that a logging category should not write any messages.
+     * Critical   - 5 - For failures that require immediate attention. Examples: data loss scenarios, out of disk space.
+     * Error      - 4 - For errors and exceptions that cannot be handled. These messages indicate a failure in the current operation or request, not an app-wide failure.
+     * Warning    - 3 - For abnormal or unexpected events. Typically includes errors or conditions that don't cause the app to fail.
+     * Information- 2 - Tracks the general flow of the app. May have long-term value.
+     * Debug      - 1 - For debugging and development. Use with caution in production due to the high volume.
+     * Trace      - 0 - Contain the most detailed messages. These messages may contain sensitive app data. These messages are disabled by default and should not be enabled in production.
+     * 
+      
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     */
+
+
+  }
+}
