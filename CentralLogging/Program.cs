9b90b15eaf59ee15adcfe55405717aa82a2842ff
Sashen <sashen50@gmail.com>
2020-08-29 13:34:31 +0200

    added nlog logging provider

--- CentralLogging/Program.cs
+++ CentralLogging/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace CentralLogging
 {
@@ -13,14 +14,36 @@ namespace CentralLogging
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      // https://github.com/NLog/NLog/wiki/Getting-started-with-ASP.NET-Core-3
+      var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+      try
+      {
+        CreateHostBuilder(args).Build().Run();
+      }
+      catch (Exception exception)
+      {
+        //NLog: catch setup errors
+        logger.Error(exception, "Stopped program because of exception");
+        throw;
+      }
+      finally
+      {
+        // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+        NLog.LogManager.Shutdown();
+      }
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+      Host.CreateDefaultBuilder(args)
+          .ConfigureWebHostDefaults(webBuilder =>
+          {
+            webBuilder.UseStartup<Startup>();
+          })
+          .ConfigureLogging(logging =>
+          {
+            logging.ClearProviders(); //remove all logging provider first because by default the framework will add Console,Debug, TraceSource and EventSource providers
+            logging.SetMinimumLevel(LogLevel.Debug);
+          })
+        .UseNLog();  // NLog: Setup NLog for Dependency injection
   }
 }
